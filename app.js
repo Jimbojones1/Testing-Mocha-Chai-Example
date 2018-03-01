91883d2565a4a7a1c2d18c0d53fb4f05ac93fa70
Jim Haff <Jimmyhaff3@gmail.com>
2018-03-01 09:14:30 -0600

    added const

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-let config     = require('config')
+const config     = require('config')
 
 
 require('./models/db')
