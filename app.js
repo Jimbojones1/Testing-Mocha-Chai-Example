9ffb461f59bc0a5a598e0678dd1750a8e2d427cd
Jim Haff <Jimmyhaff3@gmail.com>
2019-07-05 00:09:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
